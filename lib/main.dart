4f8998272102397c6622be125847ece98e13765a
이상용 <isang-yong@isang-yong-ui-MacBookPro.local>
2023-05-13 17:24:49 +0900

    add routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: MyApp()));
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => MyApp(),
+      '/check_people': (context) => Text("check_people"),
+      '/choose_menu': (context) => Text("choose_menu"),
+      '/history': (context) => Text("history"),
+      '/input_price': (context) => Text("input_price"),
+      '/result': (context) => Text("result")
+    },
+    // home: MyApp()
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -12,6 +23,7 @@ class MyApp extends StatelessWidget {
       appBar: AppBar(
           title: Text("neo nano jung san"),
           actions: [IconButton(onPressed: () {}, icon: Icon(Icons.menu))]),
+      body: Text("this is main"),
     );
   }
 }
