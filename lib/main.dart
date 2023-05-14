5adb47a7fc9d62436cacd042f753b3b57768a69c
이상용 <isang-yong@isang-yong-ui-MacBookPro.local>
2023-05-14 09:47:29 +0900

    show name list in check_people

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
     initialRoute: '/',
     routes: {
       '/': (context) => MyApp(),
-      '/check_people': (context) => CheckPeoplePage(),
+      // '/check_people': (context) => CheckPeoplePage(),
       '/choose_menu': (context) => Text("choose_menu"),
       '/history': (context) => Text("history"),
       '/input_price': (context) => Text("input_price"),
@@ -18,6 +18,23 @@ void main() {
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
   var classList = ['린더벅', '입문', '초중급', '중급', '탄탄베이직'];
+  var nameListTemp = [
+    '관장님',
+    '정시니',
+    '154꽁순',
+    '딩구',
+    '하이',
+    '바이',
+    '체리',
+    '엄마는외계인',
+    '아빠도외계인',
+    '나는지구인',
+    '난입양됐나?',
+    '빵카',
+    '네모',
+    '세모'
+  ];
+  // var routeList = ['/check_people', '/choose_menu','/history',]
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,7 +48,15 @@ class MyApp extends StatelessWidget {
                 title: TextButton(
                   child: Text(classList[i].toString()),
                   onPressed: () {
-                    Navigator.pushNamed(context, '/check_people');
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CheckPeoplePage(
+                          className: classList[i],
+                          nameList: nameListTemp,
+                        ),
+                      ),
+                    );
                   },
                 ),
               );
@@ -47,7 +72,11 @@ class MyApp extends StatelessWidget {
 }
 
 class CheckPeoplePage extends StatelessWidget {
-  const CheckPeoplePage({super.key});
+  final String className;
+  final List<String> nameList;
+
+  const CheckPeoplePage(
+      {super.key, required this.className, required this.nameList});
 
   @override
   Widget build(BuildContext context) {
@@ -55,6 +84,11 @@ class CheckPeoplePage extends StatelessWidget {
         appBar: AppBar(
             title: Text("neo nano jung san"),
             actions: [IconButton(onPressed: () {}, icon: Icon(Icons.menu))]),
-        body: Text('hi'));
+        body: ListView.builder(
+          itemCount: nameList.length,
+          itemBuilder: (c, i) {
+            return ListTile(title: Text(nameList[i]));
+          },
+        ));
   }
 }
